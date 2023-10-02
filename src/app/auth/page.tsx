591d5367e1dec85371b1ffe0e05787fdc5d2eccd
mohamadreza mosalli <mohamadresaaa@gmail.com>
2023-10-02 11:36:58 +0330

    change auth page style

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,7 +3,6 @@ import SignInForm from '@/components/forms/signInForm'
 import useAuth from '@/components/hooks/useAuth'
 import SignInWithSocials from '@/components/pages/auth/signInWithSocials'
 import Loading from '@/components/shared/loading'
-import Image from 'next/image'
 import { redirect } from 'next/navigation'
 
 export default function Auth() {
@@ -18,26 +17,15 @@ export default function Auth() {
   }
 
   return (
-    <div className='w-full h-screen flex items-start'>
-      <div className='relative w-1/6 md:w-1/2 h-full flex flex-col'>
-        <Image
-          src='/auth-cover.jpeg'
-          alt='auth-background'
-          className='w-full h-full object-cover'
-          fill
-          placeholder='blur'
-          blurDataURL='iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOU07R+AAACUwFkQasmgQAAAABJRU5ErkJggg=='
-          loading='lazy'
-        />
-      </div>
-      <div className='w-full h-full bg-slate-800 flex flex-col p-5 md:py-20 md:px-56 justify-center'>
-        <h1 className='text-xl font-bold text-gray-300 border-b-4 border-gray-300 w-48'>
-          ورود به مینتگس
-        </h1>
-        <div className='w-full flex flex-col mt-10'>
+    <div className='w-full h-screen flex items-start bg-gradient-to-r from-sky-800 from-10% to-90% to-slate-800'>
+      <div className='w-full h-full flex flex-col p-5 md:py-20 md:px-56 justify-center '>
+        <div className='w-full flex flex-col mt-10 bg-slate-600 bg-opacity-50 rounded-md p-5 shadow-md'>
+          <h1 className='text-xl font-bold text-center text-gray-300 mb-6'>
+            ورود به مینتگس
+          </h1>
           <SignInForm />
-          <div className='h-1 bg-gray-200 rounded-full mt-5 items-center text-center'>
-            <span className='relative bottom-[0.7rem] right-0 bg-slate-800 px-5 font-medium text-gray-300'>
+          <div className='border-t-4 border-gray-300 p-5 mt-5 items-center text-center'>
+            <span className='relative bottom-[0.7rem] right-0 px-5 font-medium text-gray-300'>
               ورود از طریق شبکه‌های اجتماعی
             </span>
             <SignInWithSocials />
