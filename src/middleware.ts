c1febdc1c6d22869ac5f0529606babe35993fd4a
mohamadreza mosalli <mohamadresaaa@gmail.com>
2023-10-08 20:28:12 +0330

    try middleware fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,14 @@ import { NextResponse } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+  const url = request.nextUrl.clone()
+  url.pathname = '/auth'
   console.log('has cookie', request.cookies.has('mintegs_token'))
   if (request.cookies.has('mintegs_token')) {
     return NextResponse.next()
   }
 
-  return NextResponse.redirect('/auth')
+  return NextResponse.redirect(url)
 }
 
 // See "Matching Paths" below to learn more
