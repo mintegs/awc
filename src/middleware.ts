65e3911ae01da2dc8e7ae4ce6ce058c5244bc936
mohamadreza mosalli <mohamadresaaa@gmail.com>
2023-10-08 16:12:27 +0330

    add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  console.log('has cookie', request.cookies.has('mintegs_token'))
+  return NextResponse.next()
+}
+
+// See "Matching Paths" below to learn more
+// export const config = {
+//   matcher: '/',
+// }
