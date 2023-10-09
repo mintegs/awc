a5e64aa4e3d42d4e655dcaaaf1ea52bc5d963940
mohamadreza mosalli <mohamadresaaa@gmail.com>
2023-10-09 10:40:07 +0330

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,11 +4,15 @@ import { NextResponse } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   //   console.log('has cookie', request.cookies.has('mintegs_token'))
-  //   if (request.cookies.has('mintegs_token')) {
-  //     return NextResponse.next()
-  //   }
 
-  return NextResponse.redirect(new URL('/test', request.url))
+  if (request.cookies.has('mintegs_token')) {
+    if (request.url.startsWith('/auth')) {
+      return NextResponse.redirect(new URL('/dashboard', request.url))
+    }
+    return NextResponse.next()
+  } else {
+    return NextResponse.redirect(new URL('/test', request.url))
+  }
 }
 
 // See "Matching Paths" below to learn more
