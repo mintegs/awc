897986066b664eaf595107685c26187c9bf074dc
mohamadreza mosalli <mohamadresaaa@gmail.com>
2023-10-18 20:42:19 +0330

    log data

--- src/components/forms/editArticleForm.tsx
+++ src/components/forms/editArticleForm.tsx
@@ -54,76 +54,79 @@ export default function EditArticleForm() {
           console.log(values)
         }}
       >
-        {({ dirty, isValid }) => (
-          <>
-            <Form>
-              <div className='mt-4'>
-                <div className='mb-6'>
-                  <Input
-                    name='image'
-                    type='url'
-                    placeholder='عکس مقاله را وارد کنید'
-                    isLtr
-                    withLabel
-                    label='عکس'
-                    classNames='w-full rounded-md border border-slate-700 py-3 px-5 bg-slate-700  text-white text-base outline-none focus-visible:shadow-none focus:border-blue-500 focus:border-2 transition'
-                  />
-                </div>
-                <div className='mb-6'>
-                  <Input
-                    name='title'
-                    type='text'
-                    withLabel
-                    label='عنوان'
-                    placeholder='عنوان مقاله را وارد کنید'
-                    classNames='w-full rounded-md border border-slate-700 py-3 px-5 bg-slate-700  text-white text-base outline-none focus-visible:shadow-none focus:border-blue-500 focus:border-2 transition'
-                  />
-                </div>
-                <div className='mb-6'>
-                  {loading ? (
-                    <ComboBoxSkeleton
-                      title='دسته بندی'
-                      placeholder='دسته بندی را وارد کنید'
+        {({ dirty, isValid, values }) => {
+          console.log('values', values, dirty, isValid)
+          return (
+            <>
+              <Form>
+                <div className='mt-4'>
+                  <div className='mb-6'>
+                    <Input
+                      name='image'
+                      type='url'
+                      placeholder='عکس مقاله را وارد کنید'
+                      isLtr
+                      withLabel
+                      label='عکس'
+                      classNames='w-full rounded-md border border-slate-700 py-3 px-5 bg-slate-700  text-white text-base outline-none focus-visible:shadow-none focus:border-blue-500 focus:border-2 transition'
                     />
-                  ) : (
-                    <ComboBox
-                      data={categories}
-                      filterField='title'
-                      label='دسته بندی'
-                      name='category'
-                      placeholder='دسته بندی را وارد کنید'
+                  </div>
+                  <div className='mb-6'>
+                    <Input
+                      name='title'
+                      type='text'
+                      withLabel
+                      label='عنوان'
+                      placeholder='عنوان مقاله را وارد کنید'
+                      classNames='w-full rounded-md border border-slate-700 py-3 px-5 bg-slate-700  text-white text-base outline-none focus-visible:shadow-none focus:border-blue-500 focus:border-2 transition'
                     />
-                  )}
-                </div>
-                <div className='mb-6'>
-                  <MarkdownEditor name='content' />
-                </div>
-                <div className='flex justify-between'>
-                  <button
-                    type='submit'
-                    className={`${
-                      !(dirty && isValid) ? 'cursor-not-allowed' : ''
-                    } h-10 w-32 bg-blue-600 flex justify-center items-center font-medium text-base rounded-md group text-white border-2 border-blue-600 hover:bg-slate-700 hover:text-blue-400 hover:border-blue-500 transition duration-200 shadow-lg`}
-                    disabled={!(dirty && isValid)}
-                  >
-                    {/* {isLoading ? (
+                  </div>
+                  <div className='mb-6'>
+                    {loading ? (
+                      <ComboBoxSkeleton
+                        title='دسته بندی'
+                        placeholder='دسته بندی را وارد کنید'
+                      />
+                    ) : (
+                      <ComboBox
+                        data={categories}
+                        filterField='title'
+                        label='دسته بندی'
+                        name='category'
+                        placeholder='دسته بندی را وارد کنید'
+                      />
+                    )}
+                  </div>
+                  <div className='mb-6'>
+                    <MarkdownEditor name='content' />
+                  </div>
+                  <div className='flex justify-between'>
+                    <button
+                      type='submit'
+                      className={`${
+                        !(dirty && isValid) ? 'cursor-not-allowed' : ''
+                      } h-10 w-32 bg-blue-600 flex justify-center items-center font-medium text-base rounded-md group text-white border-2 border-blue-600 hover:bg-slate-700 hover:text-blue-400 hover:border-blue-500 transition duration-200 shadow-lg`}
+                      disabled={!(dirty && isValid)}
+                    >
+                      {/* {isLoading ? (
                       <SpinnerSvg classNames={`h-5 w-5 text-white`} />
                     ) : (
                       'ویرایش'
                     )} */}
-                    ویرایش
-                  </button>
-                  <Link
-                    href='/dashboard/articles'
-                    className='h-10 w-32 bg-yellow-600 flex justify-center items-center font-medium text-base rounded-md group text-white border-2 border-yellow-600 hover:bg-slate-700 hover:text-yellow-400 hover:border-yellow-500 transition duration-200 shadow-lg'
-                  >
-                    بازگشت
-                  </Link>
+                      ویرایش
+                    </button>
+                    <Link
+                      href='/dashboard/articles'
+                      className='h-10 w-32 bg-yellow-600 flex justify-center items-center font-medium text-base rounded-md group text-white border-2 border-yellow-600 hover:bg-slate-700 hover:text-yellow-400 hover:border-yellow-500 transition duration-200 shadow-lg'
+                    >
+                      بازگشت
+                    </Link>
+                  </div>
                 </div>
-              </div>
-            </Form>
-          </>
-        )}
+              </Form>
+            </>
+          )
+        }}
       </Formik>
     </>
   )
